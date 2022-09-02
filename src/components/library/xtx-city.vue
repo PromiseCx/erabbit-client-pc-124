cbdd9bb83dab2afaeb61a1ee012f367afdcb8f9f
chen <2735569618@qq.com>
2022-09-02 14:25:31 +0800

    完成城市组件

---
+++ src/components/library/xtx-city.vue
@@ -0,0 +1,186 @@
+<template>
+  <div class="xtx-city" ref="target">
+    <div class="select" @click="toggleDialog" :class="{active}">
+      <span v-if="!fullLocation" class="placeholder">请选择配送地址</span>
+      <span v-else class="value">{{fullLocation}}</span>
+      <i class="iconfont icon-angle-down"></i>
+    </div>
+    <div class="option" v-if="active">
+      <div v-if="loading" class="loading"></div>
+      <template v-else>
+        <span  @click="changeItem(item)" class="ellipsis" v-for="item in currList" :key="item.code">{{item.name}}</span>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, ref } from 'vue'
+import { onClickOutside } from '@vueuse/core'
+import axios from 'axios'
+
+export default {
+  name: 'XtxCity',
+  props: {
+    fullLocation: {
+      type: String,
+      default: ''
+    }
+  },
+  setup (props, { emit }) {
+    const active = ref(false)
+    const loading = ref(false)
+    const allCityData = ref([])
+    const open = () => {
+      active.value = true
+      loading.value = true
+      getCityData().then(data => {
+        allCityData.value = data
+        loading.value = false
+      })
+      for (const key in changeResult) {
+        changeResult[key] = ''
+      }
+    }
+    const close = () => {
+      active.value = false
+    }
+    const toggleDialog = () => {
+      active.value ? close() : open()
+    }
+    // 点击元素之外，关闭下拉框
+    const target = ref(null)
+
+    onClickOutside(target, () => {
+      // target是监听的元素，第二参数是点击改元素外的其他地方之后触发的函数
+      close()
+    })
+
+    // 计算属性，获取当前要显示的地区列表
+    const currList = computed(() => {
+      // 默认显示省一级
+      let list = allCityData.value
+      // maybe： 市一级
+      if (changeResult.provinceCode && changeResult.provinceName) {
+        list = list.find(p => p.code === changeResult.provinceCode).areaList
+      }
+      //  maybe：地区一级
+      if (changeResult.cityCode && changeResult.cityName) {
+        list = list.find(c => c.code === changeResult.cityCode).areaList
+      }
+      return list
+    })
+
+    // 定义选择省市区数据
+    const changeResult = reactive({
+      provinceCode: '',
+      provinceName: '',
+      cityCode: '',
+      cityName: '',
+      countyCode: '',
+      countyName: '',
+      fullLocation: ''
+    })
+    // 当点击按钮的时候记录数据
+    const changeItem = (item) => {
+      // 省
+      if (item.level === 0) {
+        changeResult.provinceCode = item.code
+        changeResult.provinceName = item.name
+      }
+      // 市
+      if (item.level === 1) {
+        changeResult.cityCode = item.code
+        changeResult.cityName = item.name
+      }
+      if (item.level === 2) {
+        changeResult.countyCode = item.code
+        changeResult.countyName = item.name
+        // 通知父组件
+        changeResult.fullLocation = `${changeResult.provinceName} ${changeResult.cityName} ${changeResult.countyName}`
+        close()
+        emit('change', changeResult)
+      }
+    }
+
+    return { active, toggleDialog, target, loading, currList, changeItem }
+  }
+}
+
+// 获取省市数据
+const getCityData = () => {
+  // 1.当本地没有缓存，发请求获取数据
+  // 2.当本地有缓存，获取本地数据
+  return new Promise((resolve, reject) => {
+    // 数据存储在window上的cityData中
+    if (window.cityData) {
+      resolve(window.cityData)
+    } else {
+      const url = 'https://yjy-oss-files.oss-cn-zhangjiakou.aliyuncs.com/tuxian/area.json'
+      axios.get(url).then(res => {
+        window.cityData = res.data
+        resolve(res.data)
+      })
+    }
+  })
+}
+
+</script>
+
+<style scoped lang="less">
+.xtx-city {
+  display: inline-block;
+  position: relative;
+  z-index: 400;
+  .select {
+    border: 1px solid #e4e4e4;
+    height: 30px;
+    padding: 0 5px;
+    line-height: 28px;
+    cursor: pointer;
+    &.active {
+      background: #fff;
+    }
+    .placeholder {
+      color: #999;
+    }
+    .value {
+      color: #666;
+      font-size: 12px;
+    }
+    i {
+      font-size: 12px;
+      margin-left: 5px;
+    }
+  }
+  .option {
+    width: 542px;
+    border: 1px solid #e4e4e4;
+    position: absolute;
+    left: 0;
+    top: 29px;
+    background: #fff;
+    min-height: 30px;
+    line-height: 30px;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 10px;
+    > span {
+      width: 130px;
+      text-align: center;
+      cursor: pointer;
+      border-radius: 4px;
+      padding: 0 3px;
+      &:hover {
+        background: #f5f5f5;
+      }
+    }
+    // 省略...
+    .loading {
+      height: 290px;
+      width: 100%;
+      background: url(../../assets/images/loading.gif) no-repeat center;
+    }
+  }
+}
+</style>
--- src/views/goods/components/goods-name.vue
+++ src/views/goods/components/goods-name.vue
@@ -12,7 +12,7 @@
     </dl>
     <dl>
       <dt>配送</dt>
-      <dd>至 </dd>
+      <dd>至<XtxCity :fullLocation="fullLocation" @change="changeCity"/> </dd>
     </dl>
     <dl>
       <dt>服务</dt>
@@ -27,6 +27,7 @@
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
   name: 'GoodName',
   props: {
@@ -34,6 +35,30 @@ export default {
       type: Object,
       default: () => {}
     }
+  },
+  setup (props) {
+    // 提供给后台四项数据(没登陆)
+    const provinceCode = ref('110000')
+    const cityCode = ref('119900')
+    const countyCode = ref('110101')
+    const fullLocation = ref('北京市 市辖区 东城区')
+    // 用户已登录，取出用户收获地址中的默认地址
+    if (props.goods.userAddresses) {
+      const defaultAddress = props.goods.userAddresses?.find(item => item.isDefault === 1)
+      if (defaultAddress) {
+        provinceCode.value = defaultAddress.provinceCode
+        cityCode.value = defaultAddress.cityCode
+        countyCode.value = defaultAddress.countyCode
+        fullLocation.value = defaultAddress.fullLocation
+      }
+    }
+    const changeCity = (result) => {
+      provinceCode.value = result.provinceCode
+      cityCode.value = result.cityCode
+      countyCode.value = result.countyCode
+      fullLocation.value = result.fullLocation
+    }
+    return { fullLocation, changeCity }
   }
 }
 </script>
